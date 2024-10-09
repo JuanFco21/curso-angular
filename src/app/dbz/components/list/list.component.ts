f24805fc4d4d2657711ce00d0482d674114ec873
JuanFco21 <pacotab11@gmail.com>
2024-10-08 21:12:52 -0600

    Recibiendo información del componente padre desde un componente hijo

--- src/app/dbz/components/list/list.component.ts
+++ src/app/dbz/components/list/list.component.ts
@@ -1,11 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Character } from '../../interfaces/character.interface';
 
 @Component({
   standalone: true,
   selector: 'dbz-list',
   templateUrl: './list.component.html',
-  styleUrls: ['./list.component.css']
+  styleUrls: ['./list.component.css'],
+  imports: [
+    CommonModule
+  ]
 })
 export class ListComponent {
-
+  @Input()
+  public characterList: Character[] = [];
 }
--- src/app/dbz/pages/main-page-component.ts
+++ src/app/dbz/pages/main-page-component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
+import { Character } from '../interfaces/character.interface';
 import { ListComponent } from '../components/list/list.component';
 import { AddCharacterComponent } from '../components/add-character/add-character.component';
+
 
 @Component({
     standalone: true,
@@ -13,5 +15,14 @@ import { AddCharacterComponent } from '../components/add-character/add-character
 })
 
 export class MainPageComponent {
-
+    public characters: Character[] = [{
+        name: 'Krillin',
+        power: 1000
+    },{
+        name: 'Goku',
+        power: 9500
+    },{
+        name: 'Vegeta',
+        power: 7500
+    }];
 }
